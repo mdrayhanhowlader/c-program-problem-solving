8b44bf687f5b6aba9dcd81d9b3237803b3ec02f7
mdrayhanhowlader <rhowlader5314@gmail.com>
2024-11-25 05:41:26 +0600

    string length done

---
+++ practice/string_length.c
@@ -0,0 +1,17 @@
+// #include<stdio.h>
+// #include<string.h>
+
+// int main() {
+//     char str[15] = "Hello World!";
+
+//     int length = strlen(str);
+//     // int length = 0;
+//     // for (int i = 0; str[i] != '\0'; i++) {
+//     //     printf("%c\n", str[i]);
+//     //     length++;
+//     // }
+//     printf("Length of the string: %d\n", length); 
+//     return 0;
+// }
+
+
